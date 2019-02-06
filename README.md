d7cbae3597b4546387131f0b92db9449f780f682
Alex Pattison <alexlpattison@gmail.com>
2019-02-05 22:59:45 -0800

    Example proxy for generating signed urls on the fly

---
+++ README.md
@@ -0,0 +1,3 @@
+# flask-signed-url-example
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ app.py
@@ -0,0 +1,33 @@
+import boto3
+from flask import Flask, request, redirect
+import jwt
+import os
+
+app = Flask(__name__)
+
+secret = os.environ['SECRET']
+
+s3_client = boto3.client('s3')
+
+
+def generate_presigned_url(jwt):
+    url = s3_client.generate_presigned_url(ClientMethod='get_object', Params={
+        'Bucket': jwt['bucket'], 'Key': jwt['key']}, ExpiresIn='3600')
+    return url
+
+
+@app.route('/')
+def pull_url():
+    encoded = request.args.get('token')
+    decoded = "Please provide a valid JWT in your URL. This may mean that you used the wrong secret in the AWS console."
+    try:
+        decoded = jwt.decode(encoded, secret, algorithms=['HS256'])
+    except:
+        return decoded
+
+    generated_url = generate_presigned_url(decoded)
+    return redirect(generated_url)
+
+
+if __name__ == '__main__':
+    app.run()
